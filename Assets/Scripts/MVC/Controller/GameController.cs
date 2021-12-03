1d3ddaf98249ee62cde7b570e5c88b0eb0e103e3
89trillionJunbo <junbo@89trillion.com>
2021-12-03 12:21:30 +0800

    tips;提交新工程

---
+++ Assets/Scripts/MVC/Controller/GameController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using MVC.Model;
+using MVC.View;
+using Tools;
+using UnityEngine;
+using UnityEngine.UI;
+using Your.Namespace.Here.UniqueStringHereToAvoidNamespaceConflicts.Lists;
+
+namespace MVC.Controller
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] private RankItemPanelView mUiRankItemPanelView;
+
+        private UILeaderBoardModel uiLeaderBoardModel;
+
+        private UILeaderBoardModel UILeaderBoardModel =>
+            uiLeaderBoardModel ?? (uiLeaderBoardModel = new UILeaderBoardModel());
+
+        [SerializeField] private Text mUiCountDownTe;
+        [SerializeField] private GameObject mUiMainObj;
+        [SerializeField] private Button mUiStartBtn;
+        [SerializeField] private Transform mUiTipsTran;
+        [SerializeField] private BasicListAdapter mUiListAda;
+
+        private void Start()
+        {
+            mUiStartBtn.gameObject.SetActive(true);
+            mUiStartBtn.onClick.AddListener(() =>
+            {
+                mUiMainObj.SetActive(true);
+                StartCoroutine(CountDownFunc(UILeaderBoardModel.LeaderRankInfo.CountDown));
+                mUiStartBtn.gameObject.SetActive(false);
+            });
+
+            SetData();
+            seasonId = UILeaderBoardModel.LeaderRankInfo.SeasonID;
+            mUiListAda.EventCreateTips += ShowToast;
+            mUiListAda.Init(UILeaderBoardModel);
+        }
+
+        private int seasonId;
+
+        private void SetData()
+        {
+            mUiRankItemPanelView.SetData(UILeaderBoardModel.GetPersonDataWithRank().Item1,
+                UILeaderBoardModel.GetPersonDataWithRank().Item2);
+        }
+
+
+        private IEnumerator CountDownFunc(long endTime)
+        {
+            while (endTime > 0)
+            {
+                yield return new WaitForSeconds(1f);
+                SetData(endTime);
+                endTime--;
+            }
+        }
+
+        private void SetData(long timer)
+        {
+            var day = Mathf.FloorToInt(timer / (3600 * 24) % 30);
+            var hour = Mathf.FloorToInt(timer / 3600 % 24);
+            var minute = Mathf.FloorToInt(timer / 60 % 60);
+            var second = Mathf.FloorToInt(timer % 60);
+            mUiCountDownTe.text = $"Season {seasonId} Ranking \n Ends: {day}d {hour}h {minute}m {second}s";
+        }
+
+        public TipsPanelView tipPanelView;
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        private void ShowToast(string content)
+        {
+            if (tipPanelView.Equals(null) && tipPanelView.gameObject.activeSelf)
+            {
+                return;
+            }
+
+            tipPanelView.Init(content);
+        }
+    }
+
+    /// <summary>
+    /// 常量是文件路径及名字 下划线是资源中的名字 不属于脚本命名！！
+    /// </summary>
+    public static class Const
+    {
+        public const string FrontRankPath = "UI/rank_";
+        public const string SegmentImgPath = "UI/Segment/arenaBadge_";
+        public const string RankBgImgPath = "UI/rank list_";
+        public const string RankNormalPath = "UI/rank list_normal";
+        public const string TipsPath = "Prefabs/Toast";
+    }
+}
---
+++ Assets/Scripts/MVC/Model/UILeaderBoardModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimpleJSON;
+using UnityEngine;
+
+namespace MVC.Model
+{
+    public class UILeaderBoardModel
+    {
+        public UILeaderBoardModel()
+        {
+            ReadData();
+        }
+
+        public Tuple<PersonInfo, int> GetPersonDataWithRank(string uid = "3716954261")
+        {
+            return new Tuple<PersonInfo, int>(GetPersonData(uid),
+                GetRank(uid));
+        }
+
+        private PersonInfo GetPersonData(string uid)
+        {
+            List<PersonInfo> infoLis = null;
+
+            if (LeaderRankInfo != null)
+            {
+                infoLis = LeaderRankInfo.PersonInfos.Where(a => a.Uid == uid).ToList();
+            }
+
+            return infoLis != null && infoLis.Count > 0 ? infoLis[0] : null;
+        }
+
+        private int GetRank(string uid)
+        {
+            var rank = 0;
+            if (LeaderRankInfo != null)
+            {
+                rank = LeaderRankInfo.PersonInfos.FindIndex(item => item.Uid.Equals(uid));
+            }
+
+            return rank + 1;
+        }
+
+        public LeaderRankInfo LeaderRankInfo { get; private set; }
+
+        private const string jsonPath = "json/ranklist";
+
+        private void ReadData()
+        {
+            var json = Resources.Load<TextAsset>(jsonPath);
+            LeaderRankInfo = ReadData(json.ToString());
+        }
+
+        /// <summary>
+        /// 读取数据
+        /// </summary>
+        /// <returns></returns>
+        private static LeaderRankInfo ReadData(string data)
+        {
+            var root = JSON.Parse(data);
+
+            var leaderRankInfos = new LeaderRankInfo
+            {
+                CountDown = root["countDown"]
+            };
+
+            for (var i = 0; i < root["list"].Count; i++)
+            {
+                var personInfo = root["list"][i];
+
+                var info = new PersonInfo(
+                    personInfo["uid"],
+                    personInfo["nickName"],
+                    personInfo["trophy"].AsInt
+                );
+
+                leaderRankInfos.PersonInfos.Add(info);
+            }
+
+            leaderRankInfos.PersonInfos.Sort((a, b) => b.Trophy - a.Trophy);
+            leaderRankInfos.SeasonID = root["seasonID"].AsInt;
+            return leaderRankInfos;
+        }
+    }
+
+    public class LeaderRankInfo
+    {
+        public int CountDown;
+        public readonly List<PersonInfo> PersonInfos = new List<PersonInfo>();
+        public int SeasonID;
+    }
+
+    public class PersonInfo
+    {
+        public readonly string Uid;
+        public readonly string NickName;
+        public readonly int Trophy;
+
+        public PersonInfo(string uid, string nickName, int trophy)
+        {
+            Uid = uid;
+            NickName = nickName;
+            Trophy = trophy;
+        }
+    }
+}
